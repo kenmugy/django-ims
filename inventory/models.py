9dcfd0777abe6b928f883ebabea3ea9a033e107e
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-11-10 22:44:48 +0300

    Create laptop class

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Laptop(models.Model):
+    Obj_type = models.CharField('type',max_length = 100)
+    price = models.IntegerField(default= 0)
+    status = models.CharField(max_length = 100, default='SOLD')
+    issues = models.CharField(max_length = 100, default='No Issues')
+
+    def __str__(self):
+        return f'Type: {Obj_type} | Price: {price}'
+        
