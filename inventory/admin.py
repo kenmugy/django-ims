14f786d3f725323483b812397760b6eff430c6a9
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-11-11 00:36:19 +0300

    Re registered our models

--- inventory/admin.py
+++ inventory/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Laptop, Desktop, Mobile
 
 # Register your models here.
-admin.site.register(Laptop)
-admin.site.register(Desktop)
-admin.site.register(Mobile)
+# admin.site.register(Laptop)
+# admin.site.register(Desktop)
+# admin.site.register(Mobile)
+
+@admin.register(Laptop, Desktop, Mobile)
+class ViewAdmin(admin.ModelAdmin):
+    pass
