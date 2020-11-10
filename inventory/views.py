7a9c78e9f8f33a85e042a0c227964ae0328e37a7
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-11-11 01:02:50 +0300

    Add laptop view

--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Laptop, Desktop, Mobile
 
 # Create your views here.
 def index(request):
     return render(request, 'inventory/index.html', {})
-    
+
+def display_laptops(request):
+    context = {
+        'laptops': Laptop.objects.all(),
+        'title': 'laptops'
+    }
+    return render(request, 'inventory/index.html', context)
